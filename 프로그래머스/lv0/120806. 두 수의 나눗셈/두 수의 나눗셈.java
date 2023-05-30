e19132d0fbfb7e2b26ef4c2f0cb81bc357139035
Choi9088 <132661247+Choi9088@users.noreply.github.com>
2023-05-30 14:28:58 +0900

    Update 두 수의 나눗셈.java

--- 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -1,6 +1,6 @@
 class Solution {
     public int solution(int num1, int num2) {
-        double answer = (double)num1/num2*1000;
-        return (int)answer;
+        double answer = (double)num1/num2*1000; // int를int로 나누면 int가 되어버리므로 두 변수모두 또는 한 변수를 double로 바꿔줘야 소수점아래까지 손실없이 계산됨.
+        return (int)answer; //double을 int로 형변환시켜 소수점아래의 숫자를 제거할 수 있음.
     }
 }
