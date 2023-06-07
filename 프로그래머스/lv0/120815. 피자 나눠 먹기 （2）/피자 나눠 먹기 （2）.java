6e2a70002a8e955b25a49c7313bd8e29cbd80b68
Choi9088 <132661247+Choi9088@users.noreply.github.com>
2023-06-07 18:20:14 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.01 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
+++ 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -1,15 +1,19 @@
 class Solution {
     public int solution(int n) {
         int answer = 1;
-         while(true){
-            if(6*answer%n==0) break;
-            answer++;
+         while(answer*6%n != 0){ //6으로 나눠떨어지지않으면 나눠떨어질때까지 반복해라!
+            answer++; //나눠떨어지지 않으면 피자의 수를 계속 늘려라!
         }
         return answer;
     }
 }
 
 /*
-피자를 한 판씩 늘려갈거야 한 판을 늘릴때마다 조각의 수는 6씩 증가하겠지
-이때, 이 조각의 수를 n 명으로 나눈값이 0으로 떨어질때 까지(조각의 수%6==0)while을 반복할거야
+while문은 괄호안의 조건식이 참일경우에 계속 반복
+
+피자 한판(answer)은 6조각, 총 조각의 수(answer*6)를 n명으로 나눴을때
+1) 0이 아닌경우 피자의 수를 늘린다음 while문을 반복한다 >> (answer*6)%n !=0
+2) 0이 나온경우 그 피자의 수를 출력한다.
+
+나는 실수로 조건식을 반대로((answer*6)%n ==0)적어서 답이 안 나왔었음...
 */
