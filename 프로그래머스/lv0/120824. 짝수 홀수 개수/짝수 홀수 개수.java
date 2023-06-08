2b33b505311e72e34a5835fabae11c5a32aef16b
Choi9088 <132661247+Choi9088@users.noreply.github.com>
2023-06-09 01:29:19 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -13,3 +13,16 @@ class Solution {
         return answer;
     }
 }
+
+/* 
+다른사람의 풀이!!
+
+public int[] solution(int[] num_list) {
+        int[] answer = new int[2];
+
+        for(int i = 0; i < num_list.length; i++)
+            answer[num_list[i] % 2]++;
+
+        return answer;
+    }
+*/
