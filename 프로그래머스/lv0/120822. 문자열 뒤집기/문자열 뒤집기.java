a581106838fe0f14b94fc935511eb64dec36bd75
Choi9088 <132661247+Choi9088@users.noreply.github.com>
2023-06-09 18:34:20 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.03 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,11 +1,16 @@
 class Solution {
     public String solution(String my_string) {
-        String answer = "";
+        //2. Stringbuilder클래스 reverse()사용
+        StringBuilder answer = new StringBuilder(my_string).reverse();
+        return answer.toString();
         
+        // 1. 반복문 사용
+        /*
+        String answer = "";
         for(int i=my_string.length()-1;i>=0;i--){
             answer = answer + my_string.charAt(i);
         }
-        
         return answer;
+        */
     }
-}       
+}
