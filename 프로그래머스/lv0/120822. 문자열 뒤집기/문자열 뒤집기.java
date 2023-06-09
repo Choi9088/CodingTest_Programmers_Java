e2a54dc4053d30632cb271382507938b92982db2
Choi9088 <132661247+Choi9088@users.noreply.github.com>
2023-06-09 18:42:40 +0900

    [level 0] Title: 문자열 뒤집기, Time: 10.51 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,16 +1,24 @@
 class Solution {
     public String solution(String my_string) {
-        //2. Stringbuilder클래스 reverse()사용
-        StringBuilder answer = new StringBuilder(my_string).reverse();
-        return answer.toString();
+    // 1. 반복문 사용
         
-        // 1. 반복문 사용
-        /*
         String answer = "";
         for(int i=my_string.length()-1;i>=0;i--){
             answer = answer + my_string.charAt(i);
         }
         return answer;
+        
+        
+    //2. Stringbuilder클래스 reverse()사용
+        /*
+        StringBuilder answer = new StringBuilder(my_string).reverse();
+        return answer.toString();
+        */
+        
+    //3. StringBuffer클래스 reverse()사용
+        /*
+        StringBuffer answer = new StringBuffer(my_string).reverse();
+        return answer.toString();
         */
     }
 }
