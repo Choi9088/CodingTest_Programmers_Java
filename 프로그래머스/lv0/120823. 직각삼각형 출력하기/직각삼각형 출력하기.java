2b18569329137c449dd8f590d9cdf9100c29276e
Hyeonji Choi <132661247+Choi9088@users.noreply.github.com>
2023-07-05 16:36:33 +0900

    [level 0] Title: 직각삼각형 출력하기, Time: 182.00 ms, Memory: 66.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String star = "*";
+        
+        for(int i =0;i<n;i++){
+            System.out.println(star);
+            star += "*";
+        }
+    }
+}
+
+/*
+n개의 열 만큼 별 모양을 출력 해야하는데
+
+1~n 번까지 반복
+1번째 반복에서는 무조건 * 한개만 출력하고
+반복 될 때마다 * 하나씩을 추가시킨다
+
+*/
