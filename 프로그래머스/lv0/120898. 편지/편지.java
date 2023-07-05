76064f677344bdb275f3d01b2bdb85a289678952
Hyeonji Choi <132661247+Choi9088@users.noreply.github.com>
2023-07-06 01:45:42 +0900

    [level 0] Title: 편지, Time: 0.02 ms, Memory: 81.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120898. 편지/편지.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(String message) {
+        //message의 크기를 구해서 *2 하면 끝 아님?
+        int answer = 2 * message.length();
+        
+        return answer;
+    }
+}
