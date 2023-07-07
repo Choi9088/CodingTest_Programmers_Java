39b7cfe9abf5f1e048bce7ea189a670dbf7c0009
Hyeonji Choi <132661247+Choi9088@users.noreply.github.com>
2023-07-08 00:42:57 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 14.40 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
@@ -1,13 +1,19 @@
 class Solution {
     public String solution(String my_string, String letter) {
+        /* 1. 반복문이랑 charAt으로 */
+        String answer = "";
+        
+        for (int i=0; i<my_string.length(); i++) {
+            if (my_string.charAt(i) != letter.charAt(0)) {
+                answer += my_string.charAt(i);
+            }
+        }
+        
+        /* replace 함수 활용하기 
         String answer = my_string.replace(letter, "");
+        return answer;
+        */
+        
         return answer;
     }
 }
-
-/*replace() 메서드를 사용하여 문자를 빈 문자열로 바꾸면 Java의 문자열에서 문자의 모든 인스턴스를 제거할 수 있습니다. 다음 예제 코드는 주어진 문자열에서 소문자 "a”를 모두 제거합니다.
-
-String str = "abc ABC 123 abc";
-String strNew = str.replace("a", "");
-Output
-bc ABC 123 bc*/
