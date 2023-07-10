75c6276874a7645124b9b21dc3502f1067c1f619
Hyeonji Choi <132661247+Choi9088@users.noreply.github.com>
2023-07-10 18:44:07 +0900

    [unrated] Title: 문자열 섞기, Time: 12.63 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.java
@@ -10,3 +10,15 @@ class Solution {
         return answer;
     }
 }
+
+
+
+/*
+위 코드에서 for문안의 answer 두 줄을 단순하게 
+answer += str1.charAt(i) + str2.charAt(i);
+로 바꾸면 숫자로 변환되어 원하는 값이 나오지 않는다.
+
+아래와 같이 ""를 같이 더해 결과값이 String으로 나올 수 있게 해야한다
+answer += "" + str1.charAt(i) + str2.charAt(i);
+
+*/
