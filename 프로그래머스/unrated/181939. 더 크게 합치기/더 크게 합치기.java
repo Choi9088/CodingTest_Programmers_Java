58196b9120edc985e3fdbe80f704dfdef19856a9
Hyeonji Choi <132661247+Choi9088@users.noreply.github.com>
2023-07-10 18:35:22 +0900

    [unrated] Title: 더 크게 합치기, Time: 1.26 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.java
@@ -14,3 +14,15 @@ class Solution {
         return answer;
     }
 }
+
+/*
+ 다른사람의 풀이 일부 : 
+ 
+ int temp1 = Integer.parseInt(""+a+b);
+ 
+ 1. (""+a+b) : Int값을 String고 + 하면 String으로 자동 형 변환됨!
+ 2. Integer.parsseInt() : String값을 Int로 변환하기
+ 
+ 알고있었던 내용들인데 활용 못 했길래 메모추가!!
+ 
+ */
