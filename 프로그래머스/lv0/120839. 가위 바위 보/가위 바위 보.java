ee28005edad75c15a2b1b671075dfe31a07e09b5
Hyeonji Choi <132661247+Choi9088@users.noreply.github.com>
2023-09-23 23:38:19 +0900

    [level 0] Title: 가위 바위 보, Time: 2.86 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.java
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.java
@@ -2,19 +2,12 @@ class Solution {
     public String solution(String rsp) {
         String answer = "";
         for(int i = 0 ; i < rsp.length(); i++){
-            char temp = rsp.charAt(i);
-            
-            if(temp == '2') { 
-                temp -= 2; 
-                answer += String.valueOf(temp);
-            }
-            else if(temp == '0') { 
-                temp += 5; 
-                answer += String.valueOf(temp);
-            }
-            else { 
-                temp -= 3; 
-                answer += String.valueOf(temp); 
+            if(rsp.charAt(i) == '2') { 
+                answer += '0';
+            } else if(rsp.charAt(i) == '0') { 
+               answer += '5';
+            } else { 
+                answer += '2'; 
             }
         }
         return answer;
